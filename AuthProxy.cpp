0a4658411487fdf8bbf12b025d47c8a86f59170f
Tor Krill <tor@openproducts.se>
2014-04-13 14:26:20 +0200

    Updated Secop communication
    * Added wrapperclass to communicate with secop
    * Use wrapper for handlepassdb
    
    Signed-off-by: Tor Krill <tor@openproducts.se>

--- AuthProxy.cpp
+++ AuthProxy.cpp
@@ -19,6 +19,354 @@ using namespace std;
 using namespace Utils;
 using namespace Utils::Net;
 
+
+/* Wrapper to communicate with Secop
+ *
+ *NOTE: Not yet fully tested and no unittests written!
+ *
+ *Use with caution!
+ *
+ */
+class SecopHelper
+{
+public:
+	SecopHelper(): tid(0), secop("/tmp/secop")
+	{
+
+	}
+
+	bool Init(const string& pwd)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]= "init";
+		cmd["pwd"]=pwd;
+
+		Json::Value rep = this->DoCall(cmd);
+
+		return this->CheckReply(rep);
+	}
+
+	int Status()
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]="status";
+
+		Json::Value rep = this->DoCall(cmd);
+
+		if( this->CheckReply(rep) )
+		{
+			return rep["server"]["state"].asInt();
+		}
+		return 0;
+	}
+
+	bool SockAuth()
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]= "auth";
+		cmd["type"]="socket";
+
+		Json::Value rep = this->DoCall(cmd);
+
+		return this->CheckReply(rep);
+	}
+
+	bool PlainAuth(const string& user, const string& pwd)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]		= "auth";
+		cmd["type"]		= "plain";
+		cmd["username"]	= user;
+		cmd["password"]	= pwd;
+
+		Json::Value rep = this->DoCall(cmd);
+
+		return this->CheckReply(rep);
+	}
+
+	bool CreateUser(const string& user, const string& pwd)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]		= "createuser";
+		cmd["username"]	= user;
+		cmd["password"]	= pwd;
+
+		Json::Value rep = this->DoCall(cmd);
+
+		return this->CheckReply(rep);
+	}
+
+	bool RemoveUser(const string& user)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]		= "removeuser";
+		cmd["username"]	= user;
+
+		Json::Value rep = this->DoCall(cmd);
+
+		return this->CheckReply(rep);
+	}
+
+
+	vector<string> GetUsers()
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]= "getusers";
+
+		Json::Value rep = this->DoCall(cmd);
+
+		vector<string> users;
+		if( this->CheckReply(rep) )
+		{
+			for(auto x: rep["users"])
+			{
+				users.push_back(x.asString() );
+			}
+		}
+		return users;
+	}
+
+	vector<string> GetServices(const string& user)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]		= "getservices";
+		cmd["username"]	= user;
+
+		Json::Value rep = this->DoCall(cmd);
+
+		vector<string> services;
+		if( this->CheckReply(rep) )
+		{
+			for(auto x: rep["services"])
+			{
+				services.push_back(x.asString() );
+			}
+		}
+		return services;
+	}
+
+	bool AddService(const string& user, const string& service)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]			= "addservice";
+		cmd["username"]		= user;
+		cmd["servicename"]	= service;
+
+		Json::Value rep = this->DoCall(cmd);
+
+		return this->CheckReply(rep);
+	}
+
+	bool RemoveService(const string& user, const string& service)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]			= "removeservice";
+		cmd["username"]		= user;
+		cmd["servicename"]	= service;
+
+		Json::Value rep = this->DoCall(cmd);
+
+		return this->CheckReply(rep);
+	}
+
+	vector<string> GetACL(const string& user, const string& service)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]		= "getacl";
+		cmd["username"]	= user;
+		cmd["servicename"]	= service;
+
+		Json::Value rep = this->DoCall(cmd);
+
+		vector<string> acl;
+		if( this->CheckReply(rep) )
+		{
+			for(auto x: rep["acl"])
+			{
+				acl.push_back(x.asString() );
+			}
+		}
+		return acl;
+	}
+
+	bool AddACL(const string& user, const string& service, const string& acl)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]			= "addacl";
+		cmd["username"]		= user;
+		cmd["servicename"]	= service;
+		cmd["acl"]			= acl;
+
+		Json::Value rep = this->DoCall(cmd);
+
+		return this->CheckReply(rep);
+	}
+
+	bool RemoveACL(const string& user, const string& service, const string& acl)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]			= "removeacl";
+		cmd["username"]		= user;
+		cmd["servicename"]	= service;
+		cmd["acl"]			= acl;
+
+		Json::Value rep = this->DoCall(cmd);
+
+		return this->CheckReply(rep);
+	}
+
+	bool HasACL(const string& user, const string& service, const string& acl)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]			= "hasacl";
+		cmd["username"]		= user;
+		cmd["servicename"]	= service;
+		cmd["acl"]			= acl;
+
+		Json::Value rep = this->DoCall(cmd);
+		bool ret = false;
+
+		if ( this->CheckReply(rep) )
+		{
+			ret = rep["hasacl"].asBool();
+		}
+
+		return ret;
+	}
+
+	/* Limited, can only add key value string pairs */
+	bool AddIdentifier(const string& user, const string& service, const map<string,string>& identifier)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]			= "addidentifier";
+		cmd["username"]		= user;
+		cmd["servicename"]	= service;
+
+		for(const auto& x: identifier)
+		{
+			cmd["identifier"][ x.first ] = x.second;
+		}
+
+		Json::Value rep = this->DoCall(cmd);
+
+		return this->CheckReply(rep);
+	}
+
+	/* Identifier has to contain user &| service */
+	bool RemoveIdentifier(const string& user, const string& service, const map<string,string>& identifier)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]			= "removeidentifier";
+		cmd["username"]		= user;
+		cmd["servicename"]	= service;
+
+		for(const auto& x: identifier)
+		{
+			cmd["identifier"][ x.first ] = x.second;
+		}
+
+		Json::Value rep = this->DoCall(cmd);
+
+		return this->CheckReply(rep);
+	}
+
+	list<map<string,string>> GetIdentifiers(const string& user, const string& service)
+	{
+		Json::Value cmd(Json::objectValue);
+
+		cmd["cmd"]			= "getidentifiers";
+		cmd["username"]		= user;
+		cmd["servicename"]	= service;
+
+		Json::Value rep = this->DoCall(cmd);
+
+		list<map<string,string> > ret;
+		//logg << Logger::Debug << rep.toStyledString()<< lend;
+		if ( this->CheckReply(rep) )
+		{
+			for( auto x: rep["identifiers"] )
+			{
+				Json::Value::Members mems = x.getMemberNames();
+				map<string,string> id;
+				for( auto mem: mems)
+				{
+					id[ mem ] = x[mem].asString();
+				}
+				ret.push_back( id );
+			}
+		}
+
+		return ret;
+	}
+
+	virtual ~SecopHelper()
+	{
+
+	}
+protected:
+	Json::Value DoCall(Json::Value& cmd)
+	{
+		cmd["tid"]=this->tid;
+		cmd["version"]=1.0;
+		string r = this->writer.write( cmd );
+
+		this->secop.Write(r.c_str(), r.size() );
+
+		char buf[16384];
+		int rd;
+
+		Json::Value resp;
+
+		if( ( rd = this->secop.Read( buf, sizeof(buf) ) ) > 0  )
+		{
+
+			if( ! this->reader.parse( buf, buf+rd, resp ) )
+			{
+				logg << Logger::Error << "Failed to parse response"<<lend;
+			}
+
+		}
+
+		return resp;
+	}
+
+	bool CheckReply( const Json::Value& val )
+	{
+		bool ret = false;
+
+		if( val.isMember("status") && val["status"].isObject() )
+		{
+			ret = val["status"]["value"].asInt() == 0;
+		}
+
+		return ret;
+	}
+
+
+	int tid;
+private:
+	UnixStreamClientSocket secop;
+	Json::FastWriter writer;
+	Json::Reader reader;
+};
+
+
 AuthProxy::AuthProxy(const string &socketpath):
 	Utils::Net::NetServer(UnixStreamServerSocketPtr( new UnixStreamServerSocket(socketpath)), 0)
 {
@@ -94,71 +442,45 @@ void AuthProxy::HandlePassdb(UnixStreamClientSocketPtr sock, const string &line)
 {
 	logg << Logger::Debug << "Passdb lookup " << line << lend;
 
-	UnixStreamClientSocket secop("/tmp/secop");
-	Json::Value req;
+	SecopHelper sec;
 
-	req["tid"]=0;
-	req["version"]=1.0;
-	req["cmd"]="getidentifiers";
-	req["username"] = line;
-	req["servicename"] = "opiuser";
+	sec.SockAuth();
 
-	string r = this->writer.write( req );
+	list<map<string,string>> ids = sec.GetIdentifiers( line, "opiuser");
 
-	secop.Write(r.c_str(), r.size() );
+	if( ids.size() == 0 )
+	{
+		logg << Logger::Error << "Unable to get identifiers from backend"<<lend;
+		this->SendError(sock);
+		return;
+	}
 
-	char buf[16384];
-	int rd;
+	if( ids.front().find("password") == ids.front().end() )
+	{
+		logg << Logger::Error << "Malformed response from backend"<<lend;
+		this->SendError(sock);
+		return;
+	}
 
 	Json::Value reply;
-	reply["password"] = "test";
+	reply["password"] = this->HashPassword( ids.front()["password"] );
 	reply["userdb_home"] = "/var/opi/mail/data/"+line+"/home";
 	reply["userdb_uid"]  = 5000;
 	reply["userdb_gid"]  = 5000;
 
-	if( ( rd = secop.Read( buf, sizeof(buf) ) ) > 0  )
+	if( reply["password"].asString() == "" )
 	{
-		Json::Value resp;
-		Json::Reader reader;
-
-		if( ! reader.parse( buf, buf+rd, resp ) )
-		{
-			logg << Logger::Error << "Failed to parse response"<<lend;
-			this->SendError(sock);
-			return;
-		}
-
-		if( ! resp.isMember("status") || ! resp["status"].isMember("value") )
-		{
-			logg << Logger::Debug << "Request failed, missing arguments " <<lend;
-			this->SendError(sock);
-			return;
-		}
-
-		if( resp["status"]["value"] == 0 )
-		{
-			string hpwd = this->HashPassword( resp["identifiers"][(Json::Value::UInt)0]["password"].asString() );
-
-			if ( hpwd == "" )
-			{
-				this->SendError(sock);
-				return;
-			}
-
-			reply["password"]= hpwd;
-		}
-		else
-		{
-			logg << Logger::Debug << "Request failed "<< resp["status"]["desc"].asString() <<lend;
-			this->SendError(sock);
-			return;
-		}
-
+		logg << Logger::Error << "Unable to hash password"<<lend;
+		this->SendError(sock);
+		return;
 	}
 
 	this->SendReply(sock, reply);
 }
 
+/*
+ *TODO: Check with Secop if user exists
+ */
 void AuthProxy::HandleUserdb(UnixStreamClientSocketPtr sock, const string &line)
 {
 	logg << Logger::Debug << "Userdb lookup " << line << lend;
